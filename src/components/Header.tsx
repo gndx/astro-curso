3d91893cb85b057caa2c22d09229a65a35acb061
Oscar Barajas Tavares <gndx@MacBook-Pro-de-Oscar.local>
2024-09-25 13:09:02 -0500

    [ADD] Instalación de React

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface HeaderProps {
+    title: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+    return (
+    <header className='container mx-auto p-4'>    
+        <h1 className='text-4xl text-sky-800 font-bold'>Ladrillos.club</h1>
+    </header>
+    );
+};
+
+export default Header;
